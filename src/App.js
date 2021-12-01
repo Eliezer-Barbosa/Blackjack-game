ef01ca44db0d3ba885ee0bb0961b6895f2666eb7
Eliezer <eliezer1789j@gmail.com>
2021-12-01 21:08:39 +0000

    creating the deck of cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const deck = [
+    { stamp: "A", value: 11 },
+    { stamp: "J", value: 10 },
+    { stamp: "Q", value: 10 },
+    { stamp: "K", value: 10 },
+    { stamp: "2", value: 2 },
+    { stamp: "3", value: 3 },
+    { stamp: "4", value: 4 },
+    { stamp: "5", value: 5 },
+    { stamp: "6", value: 6 },
+    { stamp: "7", value: 7 },
+    { stamp: "8", value: 8 },
+    { stamp: "9", value: 9 },
+    { stamp: "10", value: 10 },
+  ];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const packOfCards = () => {
+    return Array(4)
+      .fill([...deck])
+      .reduce((a, b) => a.concat(b));
+  };
+
+  const getACard = () => {
+    const card =
+      packOfCards()[Math.floor(Math.random() * packOfCards().length)];
+    return card;
+  };
+
+  console.log(getACard())
+
+  return null
+};
 
 export default App;
