ec66b31bed3206af41bb31e724b8440d8739f3b2
Eliezer <eliezer1789j@gmail.com>
2021-12-18 19:35:17 +0000

    refactor - moving cards to its own file

---
+++ src/cards.js
@@ -0,0 +1,113 @@
+import clubsOf2 from "./PNG-cards-1.3/2_of_clubs.png";
+import diamondsOf2 from "./PNG-cards-1.3/2_of_diamonds.png";
+import heartsOf2 from "./PNG-cards-1.3/2_of_hearts.png";
+import spadesOf2 from "./PNG-cards-1.3/2_of_spades.png";
+import clubsOf3 from "./PNG-cards-1.3/3_of_clubs.png";
+import diamondsOf3 from "./PNG-cards-1.3/3_of_diamonds.png";
+import heartsOf3 from "./PNG-cards-1.3/3_of_hearts.png";
+import spadesOf3 from "./PNG-cards-1.3/3_of_spades.png";
+import clubsOf4 from "./PNG-cards-1.3/4_of_clubs.png";
+import diamondsOf4 from "./PNG-cards-1.3/4_of_diamonds.png";
+import heartsOf4 from "./PNG-cards-1.3/4_of_hearts.png";
+import spadesOf4 from "./PNG-cards-1.3/4_of_spades.png";
+import clubsOf5 from "./PNG-cards-1.3/5_of_clubs.png";
+import diamondsOf5 from "./PNG-cards-1.3/5_of_diamonds.png";
+import heartsOf5 from "./PNG-cards-1.3/5_of_hearts.png";
+import spadesOf5 from "./PNG-cards-1.3/5_of_spades.png";
+import clubsOf6 from "./PNG-cards-1.3/6_of_clubs.png";
+import diamondsOf6 from "./PNG-cards-1.3/6_of_diamonds.png";
+import heartsOf6 from "./PNG-cards-1.3/6_of_hearts.png";
+import spadesOf6 from "./PNG-cards-1.3/6_of_spades.png";
+import clubsOf7 from "./PNG-cards-1.3/7_of_clubs.png";
+import diamondsOf7 from "./PNG-cards-1.3/7_of_diamonds.png";
+import heartsOf7 from "./PNG-cards-1.3/7_of_hearts.png";
+import spadesOf7 from "./PNG-cards-1.3/7_of_spades.png";
+import clubsOf8 from "./PNG-cards-1.3/8_of_clubs.png";
+import diamondsOf8 from "./PNG-cards-1.3/8_of_diamonds.png";
+import heartsOf8 from "./PNG-cards-1.3/8_of_hearts.png";
+import spadesOf8 from "./PNG-cards-1.3/8_of_spades.png";
+import clubsOf9 from "./PNG-cards-1.3/9_of_clubs.png";
+import diamondsOf9 from "./PNG-cards-1.3/9_of_diamonds.png";
+import heartsOf9 from "./PNG-cards-1.3/9_of_hearts.png";
+import spadesOf9 from "./PNG-cards-1.3/9_of_spades.png";
+import clubsOf10 from "./PNG-cards-1.3/10_of_clubs.png";
+import diamondsOf10 from "./PNG-cards-1.3/10_of_diamonds.png";
+import heartsOf10 from "./PNG-cards-1.3/10_of_hearts.png";
+import spadesOf10 from "./PNG-cards-1.3/10_of_spades.png";
+import clubsOfAce from "./PNG-cards-1.3/ace_of_clubs.png";
+import diamondsOfAce from "./PNG-cards-1.3/ace_of_diamonds.png";
+import heartsOfAce from "./PNG-cards-1.3/ace_of_hearts.png";
+import spadesOfAce from "./PNG-cards-1.3/ace_of_spades2.png";
+import clubsOfJack from "./PNG-cards-1.3/jack_of_clubs.png";
+import diamondsOfJack from "./PNG-cards-1.3/jack_of_diamonds.png";
+import heartsOfJack from "./PNG-cards-1.3/jack_of_hearts.png";
+import spadesOfJack from "./PNG-cards-1.3/jack_of_spades.png";
+import clubsOfKing from "./PNG-cards-1.3/king_of_clubs.png";
+import diamondsOfKing from "./PNG-cards-1.3/king_of_diamonds.png";
+import heartsOfKing from "./PNG-cards-1.3/king_of_hearts.png";
+import spadesOfKing from "./PNG-cards-1.3/king_of_spades.png";
+import clubsOfQueen from "./PNG-cards-1.3/queen_of_clubs.png";
+import diamondsOfQueen from "./PNG-cards-1.3/queen_of_diamonds.png";
+import heartsOfQueen from "./PNG-cards-1.3/queen_of_hearts.png";
+import spadesOfQueen from "./PNG-cards-1.3/queen_of_spades.png";
+
+export const spadesSuit = [
+  { suit: "A", cardImage: spadesOfAce, value: 11 },
+  { suit: "J", cardImage: spadesOfJack, value: 10 },
+  { suit: "Q", cardImage: spadesOfQueen, value: 10 },
+  { suit: "K", cardImage: spadesOfKing, value: 10 },
+  { suit: "2", cardImage: spadesOf2, value: 2 },
+  { suit: "3", cardImage: spadesOf3, value: 3 },
+  { suit: "4", cardImage: spadesOf4, value: 4 },
+  { suit: "5", cardImage: spadesOf5, value: 5 },
+  { suit: "6", cardImage: spadesOf6, value: 6 },
+  { suit: "7", cardImage: spadesOf7, value: 7 },
+  { suit: "8", cardImage: spadesOf8, value: 8 },
+  { suit: "9", cardImage: spadesOf9, value: 9 },
+  { suit: "10", cardImage: spadesOf10, value: 10 },
+];
+export const heartsSuit = [
+  { suit: "A", cardImage: heartsOfAce, value: 11 },
+  { suit: "J", cardImage: heartsOfJack, value: 10 },
+  { suit: "Q", cardImage: heartsOfQueen, value: 10 },
+  { suit: "K", cardImage: heartsOfKing, value: 10 },
+  { suit: "2", cardImage: heartsOf2, value: 2 },
+  { suit: "3", cardImage: heartsOf3, value: 3 },
+  { suit: "4", cardImage: heartsOf4, value: 4 },
+  { suit: "5", cardImage: heartsOf5, value: 5 },
+  { suit: "6", cardImage: heartsOf6, value: 6 },
+  { suit: "7", cardImage: heartsOf7, value: 7 },
+  { suit: "8", cardImage: heartsOf8, value: 8 },
+  { suit: "9", cardImage: heartsOf9, value: 9 },
+  { suit: "10", cardImage: heartsOf10, value: 10 },
+];
+export const diamondsSuit = [
+  { suit: "A", cardImage: diamondsOfAce, value: 11 },
+  { suit: "J", cardImage: diamondsOfJack, value: 10 },
+  { suit: "Q", cardImage: diamondsOfQueen, value: 10 },
+  { suit: "K", cardImage: diamondsOfKing, value: 10 },
+  { suit: "2", cardImage: diamondsOf2, value: 2 },
+  { suit: "3", cardImage: diamondsOf3, value: 3 },
+  { suit: "4", cardImage: diamondsOf4, value: 4 },
+  { suit: "5", cardImage: diamondsOf5, value: 5 },
+  { suit: "6", cardImage: diamondsOf6, value: 6 },
+  { suit: "7", cardImage: diamondsOf7, value: 7 },
+  { suit: "8", cardImage: diamondsOf8, value: 8 },
+  { suit: "9", cardImage: diamondsOf9, value: 9 },
+  { suit: "10", cardImage: diamondsOf10, value: 10 },
+];
+export const clubsSuit = [
+  { suit: "A", cardImage: clubsOfAce, value: 11 },
+  { suit: "J", cardImage: clubsOfJack, value: 10 },
+  { suit: "Q", cardImage: clubsOfQueen, value: 10 },
+  { suit: "K", cardImage: clubsOfKing, value: 10 },
+  { suit: "2", cardImage: clubsOf2, value: 2 },
+  { suit: "3", cardImage: clubsOf3, value: 3 },
+  { suit: "4", cardImage: clubsOf4, value: 4 },
+  { suit: "5", cardImage: clubsOf5, value: 5 },
+  { suit: "6", cardImage: clubsOf6, value: 6 },
+  { suit: "7", cardImage: clubsOf7, value: 7 },
+  { suit: "8", cardImage: clubsOf8, value: 8 },
+  { suit: "9", cardImage: clubsOf9, value: 9 },
+  { suit: "10", cardImage: clubsOf10, value: 10 },
+];
